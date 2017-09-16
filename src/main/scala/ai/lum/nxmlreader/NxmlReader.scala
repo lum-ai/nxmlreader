c924422035a2f6c03148453d7365d7cef63420a8
Gus Hahn-Powell <gushahnpowell@gmail.com>
2017-09-16 11:10:21 -0700

    Replacement of newlines unnecessary
    Problem seems to be related to encoding, rather than the presence of
    newlines.

--- src/main/scala/ai/lum/nxmlreader/NxmlReader.scala
+++ src/main/scala/ai/lum/nxmlreader/NxmlReader.scala
@@ -2,9 +2,6 @@ package ai.lum.nxmlreader
 
 import java.io.{ File, InputStream, Reader }
 
-import scala.xml._
-import scala.xml.transform.RewriteRule
-
 
 class NxmlReader(val preprocessor: Preprocessor) {
   def this(
@@ -13,10 +10,7 @@ class NxmlReader(val preprocessor: Preprocessor) {
       transformText: String => String = identity
   ) = this(new NXMLPreprocessor(sectionsToIgnore, ignoreFloats, transformText))
 
-  def parse(string: String): NxmlDocument = {
-    val preprocessedText = PreprocessorUtils.preParseHook(string)
-    new NxmlDocument(XMLWithoutDTD.loadString(preprocessedText), preprocessor)
-  }
+  def parse(string: String): NxmlDocument = new NxmlDocument(XMLWithoutDTD.loadString(string), preprocessor)
   def read(name: String): NxmlDocument = new NxmlDocument(XMLWithoutDTD.loadFile(name), preprocessor)
   def read(file: File): NxmlDocument = new NxmlDocument(XMLWithoutDTD.loadFile(file), preprocessor)
   def read(inputStream: InputStream): NxmlDocument = new NxmlDocument(XMLWithoutDTD.load(inputStream), preprocessor)
